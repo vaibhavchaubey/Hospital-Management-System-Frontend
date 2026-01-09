4344b4d29e3313f447968af4105178111627fa02
Vaibhav <vaibhavchaubey786@gmail.com>
2026-01-09 17:19:33 +0530

    feat: enhance DoctorCard and PatientCard components with improved layout and icon integration

--- src/components/Admin/Doctors/DoctorCard.tsx
+++ src/components/Admin/Doctors/DoctorCard.tsx
@@ -1,5 +1,12 @@
-import { Avatar } from '@mantine/core';
+import { Avatar, Divider } from '@mantine/core';
 import { bloodGroupMap } from '../../Data/DropdownData';
+import {
+  IconBriefcase,
+  IconLicense,
+  IconMail,
+  IconMapPin,
+  IconPhone,
+} from '@tabler/icons-react';
 
 interface DoctorCardProps {
   id: number;
@@ -27,41 +34,44 @@ const DoctorCard = ({
   totalExp,
 }: DoctorCardProps) => {
   return (
-    <div className="border-0 p-3 flex flex-col gap-2 rounded-lg shadow-sm hover:shadow-md cursor-pointer">
+    <div className="border border-gray-200 p-4 flex flex-col gap-2 hover:bg-primary-50 transition duration-300 ease-in-out rounded-xl hover:shadow-[0_0_5px_1px_blue] !shadow-primary-500 cursor-pointer space-y-2">
       <div className="flex items-center gap-3">
-        <Avatar name={name} color="initials" variant="filled" />
-        <div>{name}</div>
+        <Avatar size="lg" name={name} color="initials" variant="filled" />
+        <div>
+          <div className="text-sm">{name}</div>
+          <div className="text-xs text-gray-500">
+            {specialization} &bull; {department}
+          </div>
+        </div>
       </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">Email:</div>
+      <Divider />
+      <div className="flex items-center text-xs gap-2">
+        <IconMail
+          size={24}
+          className="text-primary-700 bg-primary-100 p-1 rounded-full"
+        />
         <div>{email}</div>
       </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">DOB:</div>
-        <div>{dob}</div>
+
+      <div className="flex items-center text-xs gap-2">
+        <IconPhone
+          size={24}
+          className="text-primary-700 bg-primary-100 p-1 rounded-full"
+        />
+        <div>+91 {phone}</div>
       </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">Phone:</div>
-        <div>{phone}</div>
-      </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">Address:</div>
+      <div className="flex items-center text-xs gap-2">
+        <IconMapPin
+          size={24}
+          className="text-primary-700 bg-primary-100 p-1 rounded-full"
+        />
         <div>{address}</div>
       </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">License No:</div>
-        <div>{licenseNo}</div>
-      </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">Specialization:</div>
-        <div>{specialization}</div>
-      </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">Department:</div>
-        <div>{department}</div>
-      </div>
-      <div className="flex justify-between items-center text-sm gap-2">
-        <div className="text-gray-600">Total Experience:</div>
+      <div className="flex items-center text-xs gap-2">
+        <IconBriefcase
+          size={24}
+          className="text-primary-700 bg-primary-100 p-1 rounded-full"
+        />
         <div>{totalExp} years</div>
       </div>
     </div>
