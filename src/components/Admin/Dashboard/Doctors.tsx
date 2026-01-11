10effb69fd267dcd95158853c0617f6c9502abcf
Vaibhav <vaibhavchaubey786@gmail.com>
2026-01-11 23:18:51 +0530

    fix: correct department property casing in Doctors component

--- src/components/Admin/Dashboard/Doctors.tsx
+++ src/components/Admin/Dashboard/Doctors.tsx
@@ -1,20 +1,20 @@
 import { ScrollArea } from '@mantine/core';
-import {
-    doctors
-} from '../../Data/DashboardData';
+import { doctors } from '../../Data/DashboardData';
 
 const Doctors = () => {
-  
   const card = (app: any) => {
     return (
-      <div className="p-3 mb-3 rounded-xl justify-between border-l-4 border-violet-500 shadow-md flex bg-violet-100" key={app.id}>
+      <div
+        className="p-3 mb-3 rounded-xl justify-between border-l-4 border-violet-500 shadow-md flex bg-violet-100"
+        key={app.id}
+      >
         <div>
           <div className="font-semibold">{app.name}</div>
           <div className="text-sm text-gray-500">{app.email}</div>
         </div>
         <div className="text-right">
           <div className="text-sm text-gray-500">{app.location}</div>
-          <div className="text-sm text-gray-500">{app.Department}</div>
+          <div className="text-sm text-gray-500">{app.department}</div>
         </div>
       </div>
     );
