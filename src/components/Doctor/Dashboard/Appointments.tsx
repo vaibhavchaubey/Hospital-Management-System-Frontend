916f3d7ade6a8084e35b1fa6ad0ed776501c4b69
Vaibhav <vaibhavchaubey786@gmail.com>
2026-01-12 21:26:07 +0530

    fix: update Appointments component to display doctor instead of reason

--- src/components/Doctor/Dashboard/Appointments.tsx
+++ src/components/Doctor/Dashboard/Appointments.tsx
@@ -1,13 +1,13 @@
 import { ScrollArea } from '@mantine/core';
-import {
-    appointments
-} from '../../Data/DashboardData';
+import { appointments } from '../../Data/DashboardData';
 
 const Appointments = () => {
-  
   const card = (app: any) => {
     return (
-      <div className="p-3 mb-3 rounded-xl justify-between border-l-4 border-violet-500 shadow-md flex bg-violet-100" key={app.id}>
+      <div
+        className="p-3 mb-3 rounded-xl justify-between border-l-4 border-violet-500 shadow-md flex bg-violet-100"
+        key={app.id}
+      >
         <div>
           <div className="font-semibold">{app.patient}</div>
           <div className="text-sm text-gray-500">{app.doctor}</div>
